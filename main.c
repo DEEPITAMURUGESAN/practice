37bf87a5e3543c27d9bcabee15baf4e085561d1b
DEEPITA M J <Deepita.mj@anorasolutions.com>
2025-04-22 15:30:44 +0530

    res

--- main.c
+++ main.c
@@ -3,8 +3,11 @@ int main()
 {
     printf("hello");
     printf("this is deepita");
+<<<<<<< HEAD
     printf("2nd change");
     printf("3rd change");
     printf("4th change");
     printf("5th change");
+=======
+>>>>>>> parent of c6ff3ef (2nd change)
 }
